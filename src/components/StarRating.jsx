10a00ea4d4a816e308655e55918d9dafa00937de
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2024-08-13 08:24:40 +0530

    Update Star Component@0.1.4

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,36 +1,3 @@
-// import React, { useState } from "react";
-// import "../css properties/StarRating.css";
-
-// const StarRating = () => {
-//   const [rating, setRating] = useState(0);
-//   const [hover, setHover] = useState(0);
-
-//   const starAr = [1, 2, 3, 4, 5];
-
-//   return (
-//     <div className="star-rating">
-//       {starAr.map((star) => (
-//         <div className="star__content" key={star}>
-//           <button
-//             onClick={() => setRating(star)}
-//             onMouseOver={() => setHover(star)}
-//             onMouseLeave={() => setHover(rating)}
-//             className="btn__star"
-//           >
-//             <span
-//               className={`star ${star <= (hover || rating) ? "on" : "off"}`}
-//             >
-//               &#9733;
-//             </span>
-//           </button>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default StarRating;
-
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
